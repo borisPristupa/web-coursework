6b36af1a132b67fb074b92545c26e1da7e4fa68d
borisPristupa <boris.pristupa@gmail.com>
2019-04-10 18:45:29 +0300

    update 404 error's message format

--- src/main/java/com/ifmo/web/coursework/webservices/exception/NotFoundException.java
+++ src/main/java/com/ifmo/web/coursework/webservices/exception/NotFoundException.java
@@ -2,6 +2,6 @@ package com.ifmo.web.coursework.webservices.exception;
 
 public class NotFoundException extends RuntimeException {
     public NotFoundException(String login) {
-        super("Human not found: " + login);
+        super("Resource not found: " + login);
     }
 }
