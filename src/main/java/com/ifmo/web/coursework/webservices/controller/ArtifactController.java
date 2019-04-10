69315b1e37c4f23ddfa20fa3bba07ef10d4e016c
borisPristupa <boris.pristupa@gmail.com>
2019-04-10 16:20:57 +0300

    add creating artifacts

--- src/main/java/com/ifmo/web/coursework/webservices/controller/ArtifactController.java
+++ src/main/java/com/ifmo/web/coursework/webservices/controller/ArtifactController.java
@@ -2,16 +2,22 @@ package com.ifmo.web.coursework.webservices.controller;
 
 import com.ifmo.web.coursework.data.entity.Artifact;
 import com.ifmo.web.coursework.data.entity.Category;
+import com.ifmo.web.coursework.data.repository.AgeRepository;
 import com.ifmo.web.coursework.data.repository.ArtifactRepository;
 import com.ifmo.web.coursework.data.repository.CategoryRepository;
+import com.ifmo.web.coursework.data.repository.CountryRepository;
+import com.ifmo.web.coursework.data.utils.HumanUtils;
+import com.ifmo.web.coursework.webservices.exception.MissingRequiredArgumentException;
 import com.ifmo.web.coursework.webservices.exception.NotFoundException;
 import com.ifmo.web.coursework.webservices.response.ArtifactResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,12 +26,70 @@ import java.util.stream.Collectors;
 public class ArtifactController {
     private final ArtifactRepository artifactRepository;
     private final CategoryRepository categoryRepository;
+    private final AgeRepository ageRepository;
+    private final CountryRepository countryRepository;
+    private final HumanUtils humanUtils;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public ArtifactResponse getArtifact(@RequestParam("id") int id) {
         return ArtifactResponse.fromArtifact(artifactRepository.findById(id).orElseThrow(() ->
                 new NotFoundException("Not found artifact by id " + id)));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public ArtifactResponse addArtifact(ArtifactResponse artifactResponse) {
+        ArrayList<String> missing = new ArrayList<>();
+        if (null == artifactResponse.getType())
+            missing.add("type");
+        if (null == artifactResponse.getName())
+            missing.add("name");
+        if (null == artifactResponse.getAge())
+            missing.add("age");
+        if (null == artifactResponse.getCountry())
+            missing.add("country");
+
+        if (!missing.isEmpty())
+            throw new MissingRequiredArgumentException(missing.toArray(new String[0]));
+
+        Artifact created = new Artifact();
+
+        created.setCategoryByCategoryId(
+                categoryRepository.findByName(artifactResponse.getType())
+                        .orElseThrow(() ->
+                                new NotFoundException("Not found artifact type '" + artifactResponse.getType() + "'")));
+        created.setAgeByAgeId(
+                ageRepository.findByDescription(artifactResponse.getAge())
+                        .orElseThrow(() ->
+                                new NotFoundException("Not found age '" + artifactResponse.getAge() + "'")));
+        created.setCountryByCountryId(
+                countryRepository.findByName(artifactResponse.getCountry())
+                        .orElseThrow(() ->
+                                new NotFoundException("Not found country '" + artifactResponse.getCountry() + "'")));
+
+        created.setName(artifactResponse.getName());
+        if (null != artifactResponse.getDescription())
+            created.setDescription(artifactResponse.getDescription());
+        created.setBanned(false);
+        created.setHumanByOwner(humanUtils.getCurrentHuman());
+        created.setApproved(false);
+
+        if (Boolean.TRUE.equals(humanUtils.getCurrentHuman().getResearcher())) {
+            created.setApproved(true);
+            created.setHumanByApprover(humanUtils.getCurrentHuman());
+        }
+
+        artifactRepository.save(created);
+        artifactRepository.flush();
+        return ArtifactResponse.fromArtifact(
+                artifactRepository.findOne(Example.of(created))
+                        .orElseGet(() ->
+                                artifactRepository.findFirstByOrderByArtifactIdDesc()
+                                        .orElseThrow(() ->
+                                                new IllegalStateException("Could not save a new artifact '" +
+                                                        created.getName() + "'"))
+                        ));
     }
 
     @GetMapping("/search")
@@ -68,8 +132,11 @@ public class ArtifactController {
     }
 
     @Autowired
-    public ArtifactController(ArtifactRepository artifactRepository, CategoryRepository categoryRepository) {
+    public ArtifactController(ArtifactRepository artifactRepository, CategoryRepository categoryRepository, AgeRepository ageRepository, CountryRepository countryRepository, HumanUtils humanUtils) {
         this.artifactRepository = artifactRepository;
         this.categoryRepository = categoryRepository;
+        this.ageRepository = ageRepository;
+        this.countryRepository = countryRepository;
+        this.humanUtils = humanUtils;
     }
 }
