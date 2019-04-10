d997b204ea809cc961f843f01d5a52de9a4b5db5
borisPristupa <boris.pristupa@gmail.com>
2019-04-10 16:00:16 +0300

    made ages and categories searchable by name

--- src/main/java/com/ifmo/web/coursework/data/repository/AgeRepository.java
+++ src/main/java/com/ifmo/web/coursework/data/repository/AgeRepository.java
@@ -3,6 +3,8 @@ package com.ifmo.web.coursework.data.repository;
 import com.ifmo.web.coursework.data.entity.Age;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AgeRepository extends JpaRepository<Age, Integer> {
+import java.util.Optional;
 
+public interface AgeRepository extends JpaRepository<Age, Integer> {
+    Optional<Age> findByDescription(String description);
 }
--- src/main/java/com/ifmo/web/coursework/data/repository/CategoryRepository.java
+++ src/main/java/com/ifmo/web/coursework/data/repository/CategoryRepository.java
@@ -3,5 +3,8 @@ package com.ifmo.web.coursework.data.repository;
 import com.ifmo.web.coursework.data.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category,Integer> {
+    Optional<Category> findByName(String name);
 }
