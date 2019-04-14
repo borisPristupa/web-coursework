4f3e7d574db63838d50c4b3db3783e93246dba29
borisPristupa <boris.pristupa@gmail.com>
2019-04-14 13:47:04 +0300

    minor improvements in jms

--- src/main/java/com/ifmo/web/coursework/config/JMSConfig.java
+++ src/main/java/com/ifmo/web/coursework/config/JMSConfig.java
@@ -3,11 +3,13 @@ package com.ifmo.web.coursework.config;
 import com.rabbitmq.jms.admin.RMQConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jms.annotation.EnableJms;
 
 import javax.jms.ConnectionFactory;
 
 @Configuration
+@PropertySource("classpath:jms.properties")
 @EnableJms
 public class JMSConfig {
 
